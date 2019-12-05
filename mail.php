c1a6a4131d83312ff3c473afb017ab54acaed516
nerd35 <mukolochinedu@gmail.com>
2019-12-05 14:30:21 +0100

    contact us page added

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $Name = $_POST['Name'];
+$Email = $_POST['Email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
